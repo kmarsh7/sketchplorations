33328b18c614eb6ea9fa9616d92e170160ed7251
EC2 Default User <ec2-user@ip-172-31-14-42.us-east-2.compute.internal>
2019-06-28 14:19:52 +0000

    first commit

---
+++ app/routes.py
@@ -0,0 +1,38 @@
+from app import app
+from flask import render_template, request
+from app.models import model, formopener
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+    
+@app.route('/italy')
+def italy():
+    return render_template('italy.html')
+    
+@app.route('/USA')
+def USA():
+    return render_template('USA.html')
+    
+@app.route('/france')
+def france():
+    return render_template('france.html')
+    
+@app.route('/australia')
+def australia():
+    return render_template('australia.html')
+    
+@app.route('/china')
+def china():
+    return render_template('china.html')
+    
+@app.route('/bahamas')
+def bahamas():
+    return render_template('bahamas.html')
+    
+@app.route('/results')
+def results():
+    userdata = dict(request.form)
+    return render_template('results.html')
+    
